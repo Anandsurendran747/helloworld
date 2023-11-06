13157819c96b85a4295333217883b409c045c130
unknown <anandsurendran747@gmail.com>
2023-11-06 23:29:56 +0530

    task2

--- index.js
+++ index.js
@@ -7,10 +7,30 @@ const app =express();
 app.set('views', path.join(__dirname, 'view'));
 app.set('view engine', 'hbs');
 app.use(express.json());
+app. engine( 'hbs', hbs.engine( { extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/view/layout/', partialsDir: __dirname + '/view/partials/' } ) );
 
 app.get('/',(req,res)=>{
-    res.render('index')
+    var url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    res.render('index',{url:url})
 })
+
+app.get('/about',(req,res)=>{
+    var url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    res.render('about',{url:url})
+})
+
+app.get('/contact',(req,res)=>{
+    var url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    res.render('contact',{url:url})
+})
+
+app.get('/blog',(req,res)=>{
+    var url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    res.render('blog',{url:url})
+})
+
+
+
 
 app.listen(3000,()=>{
     console.log("server started...");
